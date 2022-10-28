c6cbf7f606e435e3115f698f510ec6917b80a47b
Alphy Gacheru <alphygacheru@gmail.com>
2022-10-28 18:47:00 +0300

    Add the solver class with a functional factorial method

---
+++ solver.rb
@@ -0,0 +1,17 @@
+class Solver
+  def factorial(num)
+    if num.zero?
+      1
+    elsif num.negative?
+      raise 'Negative numbers not allowed'
+    else
+      factorial_num = 1
+      i = 1
+      while i <= num
+        factorial_num *= i
+        i += 1
+      end
+      factorial_num
+    end
+  end
+end
